01b584d5c3fa780175a57690253b0f55c47d986e
m0nt0r <kulba.dima@mail.ru>
2026-08-02 10:06:06 +0300

    для переноса на другой пк

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -42,6 +43,21 @@ public class LoginTest extends BaseTest {
         loginPage.login("standard_user","1212121212121");
         assertEquals(loginPage.getErrorMessage(),
                 "Epic sadface: Username and password do not match any user in this service",
+                "SO BAAAD");
+    }
+    @DataProvider(name = "негативные тесты для логина")
+    public Object[][] loginData(){
+        return new Object[][]{
+                {"standard_user","","Epic sadface: Password is required"},
+                {"standard_user","1212121212121","Epic sadface: Username and password do not match any user in this service"}
+        };
+    }
+    @Test(dataProvider = "негативные тесты для логина")
+    public void login(String user, String password,String message){
+        loginPage.open();
+        loginPage.login(user,password);
+        assertEquals(loginPage.getErrorMessage(),
+                message,
                 "SO BAAAD");
     }
     @Test
